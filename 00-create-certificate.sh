3b0c6b9b27f936bc1ed567dcaba9fbe2b2c46776
Jamie Roos <jamieroos1@gmail.com>
2020-05-29 14:31:50 +0100

    Fix certificate creation to add transparency

--- 00-create-certificate.sh
+++ 00-create-certificate.sh
@@ -3,5 +3,5 @@
 AWS_REGION=us-east-1 AWS_PAGER='' aws acm request-certificate \
     --domain-name $1 \
     --validation-method DNS \
-    --idempotency-token 123456789 \
-    --options CertificateTransparencyLoggingPreference=DISABLED
+    --idempotency-token 987654 \
+    --options CertificateTransparencyLoggingPreference=ENABLED
